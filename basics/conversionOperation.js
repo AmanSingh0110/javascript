ed94458f0207bb18d2001239fe8b6751f806e08b
AmanSingh0110 <112772255+AmanSingh0110@users.noreply.github.com>
2023-10-08 11:41:28 +0000

    conversionoperation

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abs"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumbe );
-console.log(valueInNumber);
+// console.log(typeof valueInNumbe );
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abs" => NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "Aman"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -21,5 +21,24 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber); 
-console.log(typeof stringNumber);
+// console.log(stringNumber); 
+// console.log(typeof stringNumber);
+
+
+//  ************** Operations *****************  //
+
+let str1 = "Hello"
+let str2 = " Aman"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);     12
+// console.log(1 + "2");     12
+// console.log("1" + 2 + 2);  122
+// console.log(1 + 2 + "2");   32
+
+// console.log(+true);  1
+
+
+
+
