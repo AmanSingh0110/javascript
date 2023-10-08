9ee308a671d08ed39d4fa693a9d56f7c0197c960
AmanSingh0110 <112772255+AmanSingh0110@users.noreply.github.com>
2023-10-08 10:39:49 +0000

    conversionOperation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abs"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumbe );
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Aman"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Aman" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber); 
+console.log(typeof stringNumber);
